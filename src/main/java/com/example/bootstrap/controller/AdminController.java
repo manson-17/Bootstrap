8dac3f6316fb8004e7f985db24d32b42f11fafbc
manson17 <cms86@bk.ru>
2022-01-17 22:04:29 +0300

    Commit

--- src/main/java/com/example/bootstrap/controller/AdminController.java
+++ src/main/java/com/example/bootstrap/controller/AdminController.java
@@ -6,7 +6,6 @@ import com.example.bootstrap.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
